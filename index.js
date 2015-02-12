adb3309da9c07fff560494b0919ad3ef7fe118df
Touko Vainio-Kaila <touko.vk@gmail.com>
2015-02-12 06:53:43 +0200

    Postgres connection POC

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var pg = require('pg');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -7,6 +8,18 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
   response.send('Hello World!!');
 });
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
