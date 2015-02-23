e60edc9aebc6c1c57cc58a5ec3142b424ac0d5d4
Touko Vainio-Kaila <touko.vk@gmail.com>
2015-02-23 16:26:25 +0200

    Use squel.js with db.js

--- db.js
+++ db.js
@@ -1,20 +1,13 @@
 var pg = require('pg');
+var squel = require('squel');
 
 function getEvents(apiQuery, callback) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    var wheres = [];
-    var params = [];
-    var sqlQuery;
+    var sql = squel.select().from('event');
     if(apiQuery.firstDate) {
-      // TODO - API query parameter validation?
-      wheres.push('start_time > $'+ wheres.length + 1);
-      params.push(apiQuery.firstDate);
+      sql = sql.where('start_time > ?', apiQuery.firstDate);
     }
-    sqlQuery = 'SELECT * FROM event';
-    if(wheres.length > 0) {
-      sqlQuery += ' WHERE ' + wheres.join(', ');
-    }
-    client.query(sqlQuery, params, function(err, result) {
+    client.query(sql.toString(), sql.toParam(), function(err, result) {
       done();
       if (err) {
         callback(err);
