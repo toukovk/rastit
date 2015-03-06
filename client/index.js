687f18a8c9a5ebae2b1dfd24e9e091c6afab9cf2
Touko Vainio-Kaila <touko.vk@gmail.com>
2015-03-06 17:30:02 +0200

    Date picker Bacon binding POC

--- client/index.js
+++ client/index.js
@@ -1,14 +1,25 @@
 var Bacon = require("baconjs");
+var $ = require("jquery");
+var bjq = require("bacon.jquery");
 var Pikaday = require('pikaday');
-var domready = require('domready');
 
 module.exports = {
   init: function () {
-    var field = document.getElementById('datepicker');
-    var picker = new Pikaday({ field: field });
+    var firstDateField = $('#datepicker');
+    var defaultDate = new Date();
+    var picker = new Pikaday({
+      field: firstDateField[0],
+      format: 'D.M.YYYY'
+    });
+
+    firstDateField.asEventStream('change').onValue(function(event) {
+      console.log('Date value changed to', event.target.value);
+    });
+
+    picker.setDate(defaultDate);
   }
 };
 
-domready(function() {
+$(function() {
   module.exports.init();
 });
