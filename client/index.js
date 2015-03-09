6a45996b3498d3e90be6c9ba192cd822ed6bc782
Touko Vainio-Kaila <touko.vk@gmail.com>
2015-03-09 18:42:44 +0200

    Ajax call to events API

--- client/index.js
+++ client/index.js
@@ -3,25 +3,29 @@ var $ = require("jquery");
 var bjq = require("bacon.jquery");
 var Pikaday = require('pikaday');
 var moment = require('moment');
+var _ = require('lodash');
 
 module.exports = {
   init: function () {
+    var uiDateFormat = 'D.M.YYYY';
+    var apiDateFormat = 'YYYY-MM-DD';
+
     var firstDateField = $('#first-date');
     var lastDateField = $('#last-date');
     var today = moment();
     var firstDatePicker = new Pikaday({
       field: firstDateField[0],
-      format: 'D.M.YYYY'
+      format: uiDateFormat
     });
     var lastDatePicker = new Pikaday({
       field: lastDateField[0],
-      format: 'D.M.YYYY'
+      format: uiDateFormat
     });
     var getEventTargetValue = function(event) {
       return event.target.value;
     }
     var pickerDateStringToMoment = function(dateStr) {
-      return moment(dateStr, 'D.M.YYYY');
+      return moment(dateStr, uiDateFormat);
     }
     var pickerElementToStream = function(fieldElement) {
       return fieldElement.asEventStream('change').map(getEventTargetValue).map(pickerDateStringToMoment);
@@ -33,8 +37,16 @@ module.exports = {
       firstDate: firstDate,
       lastDate: lastDate
     });
+    var events = terms.flatMapLatest(function(terms) {
+      var query = '/db?firstDate=' + terms.firstDate.format(apiDateFormat) + '&lastDate=' + terms.lastDate.format(apiDateFormat);
+      return Bacon.fromPromise($.ajax(query));
+    }).mapError("Search fail");
 
-    terms.log('Terms');
+    events.onValue(function(events) {
+      var compiled = _.template('<li><%= info %> (<%= organizer %>)</li>');
+      var foo = _.map(events, compiled).join('');
+      $('ul#events').html(foo);
+    });
 
     firstDatePicker.setMoment(today);
     lastDatePicker.setMoment(today.add(2, 'weeks'));
