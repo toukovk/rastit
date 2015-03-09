da0b00a332aa44f355a93618473d55d4c975100e
Touko Vainio-Kaila <touko.vk@gmail.com>
2015-03-09 16:25:39 +0200

    Dates & Bacon POC with first & last date

--- client/index.js
+++ client/index.js
@@ -2,21 +2,42 @@ var Bacon = require("baconjs");
 var $ = require("jquery");
 var bjq = require("bacon.jquery");
 var Pikaday = require('pikaday');
+var moment = require('moment');
 
 module.exports = {
   init: function () {
-    var firstDateField = $('#datepicker');
-    var defaultDate = new Date();
-    var picker = new Pikaday({
+    var firstDateField = $('#first-date');
+    var lastDateField = $('#last-date');
+    var today = moment();
+    var firstDatePicker = new Pikaday({
       field: firstDateField[0],
       format: 'D.M.YYYY'
     });
+    var lastDatePicker = new Pikaday({
+      field: lastDateField[0],
+      format: 'D.M.YYYY'
+    });
+    var getEventTargetValue = function(event) {
+      return event.target.value;
+    }
+    var pickerDateStringToMoment = function(dateStr) {
+      return moment(dateStr, 'D.M.YYYY');
+    }
+    var pickerElementToStream = function(fieldElement) {
+      return fieldElement.asEventStream('change').map(getEventTargetValue).map(pickerDateStringToMoment);
+    }
 
-    firstDateField.asEventStream('change').onValue(function(event) {
-      console.log('Date value changed to', event.target.value);
+    var firstDate = pickerElementToStream(firstDateField);
+    var lastDate = pickerElementToStream(lastDateField);
+    var terms = Bacon.combineTemplate({
+      firstDate: firstDate,
+      lastDate: lastDate
     });
 
-    picker.setDate(defaultDate);
+    terms.log('Terms');
+
+    firstDatePicker.setMoment(today);
+    lastDatePicker.setMoment(today.add(2, 'weeks'));
   }
 };
 
